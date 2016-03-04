9846456e229e0ff7a059c59a55316f502dd2bc19
Gabriel Vasconcelos <gfreivasc@gmail.com>
2016-03-04 01:37:36 -0300

    Test for different passwords entry

--- rawauth/tests.py
+++ rawauth/tests.py
@@ -10,8 +10,8 @@ class AuthorTest(TestCase):
 
 class AuthorCreateViewTest(TestCase):
 
-    def test_user_logs_in_after_creation(self):
-        post_data = {
+    def setUp(self):
+        self.post_data = {
             'username': 'test',
             'email': 'te.st@te.st',
             'first_name': 'Test',
@@ -20,8 +20,18 @@ class AuthorCreateViewTest(TestCase):
             'password2': 'test123',
         }
 
-        response = self.client.post(reverse('rawauth:registration'), post_data)
+    def test_user_logs_in_after_creation(self):
+        response = self.client.post(reverse('rawauth:registration'),
+                                    self.post_data)
         self.assertEqual(response.status_code, 302)
 
         response = self.client.get(reverse('blog:new'))
         self.assertEqual(response.status_code, 200)
+
+    def test_fails_passwords_dont_match(self):
+        post_data = self.post_data
+        post_data['password2'] = 'unmatching'
+
+        response = self.client.post(reverse('rawauth:registration'),
+                                    self.post_data)
+        self.assertContains(response, "Passwords don&#39;t match!")
