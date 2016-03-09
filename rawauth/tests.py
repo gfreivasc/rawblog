aaba8b9505b9089315f79308b313711dd4a980b9
Gabriel Vasconcelos <gfreivasc@gmail.com>
2016-03-09 13:11:07 -0300

    Added few model tests

--- rawauth/tests.py
+++ rawauth/tests.py
@@ -1,11 +1,22 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from rawauth.models import Author
 
 
 class AuthorTest(TestCase):
 
-    def test_case(self):
-        pass
+    def setUp(self):
+        self.author = Author.objects.create_user('Meme', 'meme@dot.com',
+                                                 'tester')
+        self.author.save()
+
+    def test_author_created_successfully(self):
+        self.assertNotEqual(len(Author.objects.all()), 0)
+
+    def test_author_logs_in(self):
+        self.client.login(username='Meme', password='tester')
+        self.assertEqual(int(self.client.session['_auth_user_id']),
+                         self.author.pk)
 
 
 class AuthorCreateViewTest(TestCase):
@@ -20,13 +31,12 @@ class AuthorCreateViewTest(TestCase):
             'password2': 'test123',
         }
 
-    def test_user_logs_in_after_creation(self):
+    def test_author_logs_in_after_creation(self):
         response = self.client.post(reverse('rawauth:registration'),
                                     self.post_data)
         self.assertEqual(response.status_code, 302)
 
-        response = self.client.get(reverse('blog:new'))
-        self.assertEqual(response.status_code, 200)
+        self.assertIn('_auth_user_id', self.client.session)
 
     def test_fails_passwords_dont_match(self):
         post_data = self.post_data
@@ -36,7 +46,7 @@ class AuthorCreateViewTest(TestCase):
                                     self.post_data)
         self.assertContains(response, "Passwords don&#39;t match!")
 
-    def test_user_registration_redirects_to_next(self):
+    def test_author_registration_redirects_to_next(self):
         response = self.client.post(
             reverse('rawauth:registration')+'?next='+reverse('blog:new'),
             self.post_data
